ee15882215fc9c04ffb416bb2fcbaf6ee4c0c18d
Philippe Eng <ilyfing@gmail.com>
2019-02-23 15:11:43 +0100

    [dev] Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/standard'],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': ['error', 'never']
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   }
-};
+}
